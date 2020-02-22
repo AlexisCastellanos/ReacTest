ce262e097f7d5ca43942373447f7f4199c208ed4
Alexis Castellanos <intelaki@gmail.com>
2020-02-21 23:42:07 -0600

    Updates

---
+++ precache-manifest.52656eb3d65a172a1b9a0ced89d87ebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e51e18b0be8491f85f8562e4645832",
+    "url": "/ReacTest/index.html"
+  },
+  {
+    "revision": "13230a644c1451e5bf83",
+    "url": "/ReacTest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "86ff9007e2e7ad1d710d",
+    "url": "/ReacTest/static/js/2.b037ad82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReacTest/static/js/2.b037ad82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13230a644c1451e5bf83",
+    "url": "/ReacTest/static/js/main.c5a58ea5.chunk.js"
+  },
+  {
+    "revision": "3ef9abca6ad6def10252",
+    "url": "/ReacTest/static/js/runtime-main.639d69c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReacTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ReacTest/precache-manifest.52656eb3d65a172a1b9a0ced89d87ebb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReacTest/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
